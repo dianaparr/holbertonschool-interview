97a66e4f4238f9b2eb4977f3dfcba049faea069d
dianaparr <dianaparra.9611@gmail.com>
2021-12-11 19:58:21 -0500

    PEP8 style

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """ Module name 0-lockboxes """
 
+
 def canUnlockAll(boxes):
     """ This exercise is the one example for the job interview.
         You have n number of locked boxes in front of you. Each box is
-        numbered sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
+        numbered sequentially from 0 to n - 1 and each box may contain keys to
+        the other boxes.
 
         Write a method that determines if all the boxes can be opened.
 
@@ -14,21 +16,23 @@ def canUnlockAll(boxes):
         Return:
             True if all boxes can be opened, else return False
     """
-    #unordered collection of unique elements
+    # unordered collection of unique elements
     keysBoxes = set()
 
-    #initial check
+    # initial check
     if boxes is None or len(boxes) == 0:
         return False
 
-    #method adds a counter to an iterable and returns it in a form of enumerating object
+    # method adds a counter to an iterable and returns it in a form
+    # of enumerating object.
     for nextKey, box in enumerate(boxes):
         nextKey += 1
         for key in box:
             if key < len(boxes):
                 keysBoxes.add(key)
                 keysBoxes.update(boxes[key])
-            if len(keysBoxes) == len(boxes) or len(keysBoxes) == len(boxes) - 1:
+            if len(keysBoxes) == len(boxes) or len(
+                      keysBoxes) == len(boxes) - 1:
                 return True
         if nextKey not in keysBoxes:
             return False
