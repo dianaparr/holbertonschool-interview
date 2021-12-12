0139f9afb9b9f148e9a840eae51ae269fe360c79
dianaparr <dianaparra.9611@gmail.com>
2021-12-11 19:48:53 -0500

    Add exercise lockboxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+""" Module name 0-lockboxes """
+
+def canUnlockAll(boxes):
+    """ This exercise is the one example for the job interview.
+        You have n number of locked boxes in front of you. Each box is
+        numbered sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
+
+        Write a method that determines if all the boxes can be opened.
+
+        Parameter:
+            boxes ->  list of lists
+
+        Return:
+            True if all boxes can be opened, else return False
+    """
+    keysBoxes = set() #unordered collection of unique elements
+
+    #initial check
+    if boxes is None or len(boxes) == 0:
+        return False
+
+    for nextKey, box in enumerate(boxes): #method adds a counter to an iterable and returns it in a form of enumerating object
+        nextKey += 1
+        for key in box:
+            if key < len(boxes):
+                keysBoxes.add(key)
+                keysBoxes.update(boxes[key])
+            if len(keysBoxes) == len(boxes) or len(keysBoxes) == len(boxes) - 1:
+                return True
+        if nextKey not in keysBoxes:
+            return False
