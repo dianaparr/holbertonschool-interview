08f511e6b1d3b294f46d0bfa3f6e0e058eba5e34
dianaparr <dianaparra.9611@gmail.com>
2021-12-23 09:50:42 -0500

    fix betty style

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -10,17 +10,17 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node = NULL;
+	binary_tree_t *new_node = NULL;
 
-    /* allocate new node in memory */
-    new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-        return NULL;
+	/* allocate new node in memory */
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->parent = parent;
-    new_node->n = value;
-    new_node->left = NULL;
-    new_node->right = NULL;
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
 
-    return new_node;
+	return (new_node);
 }
