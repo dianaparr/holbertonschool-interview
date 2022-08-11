aa606f0850270fe56839cb7ecebbc5efda86c36c
dianaparr <dianaparra.9611@gmail.com>
2022-08-10 23:11:34 -0500

    add task interview API star wars

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const request = require('request');
+const { argv } = require('process');
+
+const url = `https://swapi-api.hbtn.io/api/films/${argv[2]}/`;
+
+async function getNames(url) {
+  return new Promise((resolve) => {
+    request(url, (_err, _res, body) => {
+      resolve(JSON.parse(body).name);
+    });
+  });
+}
+
+async function showNames(_err, _res, body) {
+  for (const u of JSON.parse(body).characters) {
+    console.log(await getNames(u));
+  }
+}
+
+request(url, showNames);
