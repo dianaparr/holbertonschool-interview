35fc2f5d32f86f8002634bee8e46501555291930
dianaparr <dianaparra.9611@gmail.com>
2022-09-22 00:08:53 -0500

    add advanced task

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,11 @@
+#ifndef _INFINITE_H_
+#define _INFINITE_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int _putchar(char c);
+
+#endif /* _INFINITE_H_ */
