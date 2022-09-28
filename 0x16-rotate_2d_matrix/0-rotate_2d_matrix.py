5bd9805d0ce6fd8dfe84c4d0dd71032810c9deeb
dianaparr <dianaparra.9611@gmail.com>
2022-09-28 18:29:00 -0500

    add interview task

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Task Rotate 2D Matrix """
+
+
+def rotate_2d_matrix(matrix):
+    """ Rotate 2D Matrix
+        Given an n x n 2D matrix, rotate it 90 degrees clockwise.
+
+        Do not return anything. The matrix must be edited in-place.
+    """
+    lenMatrix = len(matrix)
+    for i in range(lenMatrix//2):
+        for j in range(i, lenMatrix-1):
+            matrix[i][j],
+            matrix[j][lenMatrix-1] = matrix[j][lenMatrix-1], matrix[i][j]
+            matrix[i][j],
+            matrix[lenMatrix-1][lenMatrix-1-j+i] =
+            matrix[lenMatrix-1][lenMatrix-1-j+i], matrix[i][j]
+            matrix[i][j], matrix[lenMatrix-1-j+i][i] =
+            matrix[lenMatrix-1-j+i][i], matrix[i][j]
+        lenMatrix -= 1
