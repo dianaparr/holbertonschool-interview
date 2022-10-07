8a7a04a8e99a0b0fa8b57f1a11d6edf046b4f292
dianaparr <dianaparra.9611@gmail.com>
2022-10-06 21:53:10 -0500

    fix betty style

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -9,32 +9,32 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *h, *t;
+	listint_t *h, *t;
 
-    if (!head)
-        return (NULL);
+	if (!head)
+		return (NULL);
 
-    h = t = head;
+	h = t = head;
 
-    while (t && h && h->next)
-    {
-        t = t->next;
-        h = (h->next)->next;
+	while (t && h && h->next)
+	{
+		t = t->next;
+		h = (h->next)->next;
 
-        if (h == t)
-        {
-            t = head;
+		if (h == t)
+		{
+			t = head;
 
-            while (t && h)
-            {
-                if (t == h)
-                    return (t);
+			while (t && h)
+			{
+				if (t == h)
+					return (t);
 
-                t = t->next;
-                h = h->next;
-            }
-        }
-    }
+				t = t->next;
+				h = h->next;
+			}
+		}
+	}
 
-    return (NULL);
+	return (NULL);
 }
