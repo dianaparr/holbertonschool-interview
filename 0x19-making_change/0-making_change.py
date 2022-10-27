488fccfc1155e131203f2b7dfe4b09519948f594
dianaparr <dianaparra.9611@gmail.com>
2022-10-26 19:22:13 -0500

    fix task efficiency

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ Making change"""
-import sys
 
 
 def makeChange(coins, total):
@@ -9,18 +8,18 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    max_n = sys.maxsize
-    hash_t = [max_n for i in range(total + 1)]
-    hash_t[0] = 0
-    lenCoins = len(coins)
-    for d in range(1, total + 1):
-        for i in range(lenCoins):
-            if coins[i] <= d:
-                res = hash_t[d - coins[i]]
-                if res != max_n and res + 1 < hash_t[d]:
-                    hash_t[d] = res + 1
+    coins.sort()
+    coins.reverse()
+    fewest = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        buff = total // coin
+        fewest += buff
+        total -= (buff * coin)
 
-    if hash_t[total] == max_n:
+    if total != 0:
         return -1
 
-    return hash_t[total]
+    return fewest
+
